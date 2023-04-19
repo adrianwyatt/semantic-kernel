88acdd27a17b1e17079f9ca4615b3ef414316a6a
lemiller@microsoft.com <lemiller@microsoft.com>
2023-04-19 01:11:14 -0700

    Refactor Plan class and add Function Flow planner and parser
          Summary: This commit introduces several changes to the Plan class, which is used to represent and execute a sequence of steps in the Semantic Kernel. The main changes are:
    
          - The Plan class now uses a list of Plan objects as steps instead of ISKFunction objects. This allows for more flexibility and readability in the plan representation.
          - The Plan class now supports serialization and deserialization from JSON, using the SKContext to register the functions in the plan.
          - The Plan class now has methods to get the next step variables for a nested plan, to update the state variables after each step execution, and to handle named parameters, outputs, and settings.
          - The commit also adds a new planner that uses the Function Flow semantic function to create a plan for a given goal. The planner uses a restricted skill name to avoid invoking itself recursively.
          - The commit also adds a new parser that can parse XML plans created by the Function Flow function. The parser converts the XML string to a Plan object that can be executed by the semantic kernel. The parser handles various tags and attributes in the XML plan, such as goal, function, setContextVariable, and appendToResult.
          - The commit also adds a PlannerConfig class that holds common configuration options for planners, and updates the SemanticFunctionConstants class with the definition of the Function Flow function.
          - The commit also adds documentation comments for some of the methods and properties in the Plan class and related classes.

--- dotnet/src/SemanticKernel/Orchestration/Plan.cs
+++ dotnet/src/SemanticKernel/Orchestration/Plan.cs
@@ -2,12 +2,15 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.SemanticKernel.AI.TextCompletion;
+using Microsoft.SemanticKernel.Diagnostics;
 using Microsoft.SemanticKernel.SkillDefinition;
 
 namespace Microsoft.SemanticKernel.Orchestration;
@@ -16,29 +19,72 @@ namespace Microsoft.SemanticKernel.Orchestration;
 /// Standard Semantic Kernel callable plan.
 /// Plan is used to create trees of <see cref="ISKFunction"/>s.
 /// </summary>
-public class Plan : ISKFunction
+public sealed class Plan : ISKFunction
 {
+    /// <summary>
+    /// Converter for <see cref="ContextVariables"/> to/from JSON.
+    /// </summary>
+    public class ContextVariablesConverter : JsonConverter<ContextVariables>
+    {
+        /// <summary>
+        /// Read the JSON and convert to ContextVariables
+        /// </summary>
+        /// <param name="reader">The JSON reader.</param>
+        /// <param name="typeToConvert">The type to convert from.</param>
+        /// <param name="options">The JSON serializer options.</param>
+        /// <returns>The deserialized <see cref="ContextVariables"/>.</returns>
+        public override ContextVariables Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var keyValuePairs = JsonSerializer.Deserialize<IEnumerable<KeyValuePair<string, string>>>(ref reader, options);
+            var context = new ContextVariables();
+
+            foreach (var kvp in keyValuePairs!)
+            {
+                context.Set(kvp.Key, kvp.Value);
+            }
+
+            return context;
+        }
+
+        public override void Write(Utf8JsonWriter writer, ContextVariables value, JsonSerializerOptions options)
+        {
+            JsonSerializer.Serialize(writer, value, options);
+        }
+    }
+
     /// <summary>
     /// State of the plan
     /// </summary>
     [JsonPropertyName("state")]
+    [JsonConverter(typeof(ContextVariablesConverter))]
     public ContextVariables State { get; } = new();
 
     /// <summary>
     /// Steps of the plan
     /// </summary>
     [JsonPropertyName("steps")]
-    internal IReadOnlyList<ISKFunction> Steps => this._steps.AsReadOnly();
+    public IReadOnlyList<Plan> Steps => this._steps.AsReadOnly();
+    // This doesn't work with json deserialization
 
     /// <summary>
     /// Named parameters for the function
     /// </summary>
     [JsonPropertyName("named_parameters")]
+    [JsonConverter(typeof(ContextVariablesConverter))]
     public ContextVariables NamedParameters { get; set; } = new();
 
-    public bool HasNextStep => this.NextStepIndex < this.Steps.Count;
+    /// <summary>
+    /// Named outputs for the function
+    /// </summary>
+    [JsonPropertyName("named_outputs")]
+    [JsonConverter(typeof(ContextVariablesConverter))]
+    public ContextVariables NamedOutputs { get; set; } = new();
 
-    protected int NextStepIndex { get; set; } = 0;
+    /// <summary>
+    /// Gets whether the plan has a next step.
+    /// </summary>
+    [JsonIgnore]
+    public bool HasNextStep => this.NextStepIndex < this.Steps.Count;
 
     #region ISKFunction implementation
 
@@ -55,11 +101,11 @@ public class Plan : ISKFunction
     public string Description { get; set; } = string.Empty;
 
     /// <inheritdoc/>
-    [JsonPropertyName("is_semantic")]
+    [JsonIgnore]
     public bool IsSemantic { get; internal set; } = false;
 
     /// <inheritdoc/>
-    [JsonPropertyName("request_settings")]
+    [JsonIgnore]
     public CompleteRequestSettings RequestSettings { get; internal set; } = new();
 
     #endregion ISKFunction implementation
@@ -80,8 +126,17 @@ public class Plan : ISKFunction
     /// </summary>
     /// <param name="goal">The goal of the plan used as description.</param>
     /// <param name="steps">The steps to add.</param>
-    [JsonConstructor]
     public Plan(string goal, params ISKFunction[] steps) : this(goal)
+    {
+        this.AddSteps(steps);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Plan"/> class with a goal description and steps.
+    /// </summary>
+    /// <param name="goal">The goal of the plan used as description.</param>
+    /// <param name="steps">The steps to add.</param>
+    public Plan(string goal, params Plan[] steps) : this(goal)
     {
         this.AddSteps(steps);
     }
@@ -96,6 +151,42 @@ public class Plan : ISKFunction
     }
 
     /// <summary>
+    /// Initializes a new instance of the <see cref="Plan"/> class with a function and steps.
+    /// </summary>
+    /// <param name="name">The name of the plan.</param>
+    /// <param name="skillName">The name of the skill.</param>
+    /// <param name="description">The description of the plan.</param>
+    /// <param name="state">The state of the plan.</param>
+    /// <param name="namedParameters">The named parameters of the plan.</param>
+    /// <param name="namedOutputs">The named outputs of the plan.</param>
+    /// <param name="steps">The steps of the plan.</param>
+    [JsonConstructor]
+    public Plan(string name, string skillName, string description, ContextVariables state, ContextVariables namedParameters, ContextVariables namedOutputs,
+        IReadOnlyList<Plan> steps)
+    {
+        this.Name = name;
+        this.SkillName = skillName;
+        this.Description = description;
+        this.State = state;
+        this.NamedParameters = namedParameters;
+        this.NamedOutputs = namedOutputs;
+        this._steps.Clear();
+        this.AddSteps(steps.ToArray());
+    }
+
+    /// <summary>
+    /// Adds one or more existing plans to the end of the current plan as steps.
+    /// </summary>
+    /// <param name="steps">The plans to add as steps to the current plan.</param>
+    /// <remarks>
+    /// When you add a plan as a step to the current plan, the steps of the added plan are executed after the steps of the current plan have completed.
+    /// </remarks>
+    public void AddSteps(params Plan[] steps)
+    {
+        this._steps.AddRange(steps);
+    }
+
+    /// <summary>
     /// Adds one or more new steps to the end of the current plan.
     /// </summary>
     /// <param name="steps">The steps to add to the current plan.</param>
@@ -104,10 +195,7 @@ public class Plan : ISKFunction
     /// </remarks>
     public void AddSteps(params ISKFunction[] steps)
     {
-        foreach (var step in steps)
-        {
-            this._steps.Add(new Plan(step));
-        }
+        this._steps.AddRange(steps.Select(step => new Plan(step)));
     }
 
     /// <summary>
@@ -137,7 +225,7 @@ public class Plan : ISKFunction
     public FunctionView Describe()
     {
         // TODO - Eventually, we should be able to describe a plan and it's expected inputs/outputs
-        return this.Function?.Describe() ?? throw new NotImplementedException();
+        return this.Function?.Describe() ?? new();
     }
 
     /// <inheritdoc/>
@@ -174,7 +262,6 @@ public class Plan : ISKFunction
             while (this.HasNextStep)
             {
                 var functionContext = context;
-                // Loop through State and add anything missing to functionContext
 
                 AddVariablesToContext(this.State, functionContext);
 
@@ -225,22 +312,193 @@ public class Plan : ISKFunction
         {
             var step = this.Steps[this.NextStepIndex];
 
-            context = await step.InvokeAsync(context);
+            var functionVariables = this.GetNextStepVariables(context.Variables, step);
+            var functionContext = new SKContext(functionVariables, context.Memory, context.Skills, context.Log, context.CancellationToken);
 
-            if (context.ErrorOccurred)
+            var keysToIgnore = functionVariables.Select(x => x.Key).ToList(); // when will there be new keys added? that's output kind of?
+
+            var result = await step.InvokeAsync(functionContext);
+
+            if (result.ErrorOccurred)
             {
                 throw new KernelException(KernelException.ErrorCodes.FunctionInvokeError,
                     $"Error occurred while running plan step: {context.LastErrorDescription}", context.LastException);
             }
 
+            #region Update State
+
+            foreach (var key in functionVariables.Select(x => x.Key))
+            {
+                if (!keysToIgnore.Contains(key, StringComparer.InvariantCultureIgnoreCase) && functionVariables.Get(key, out var value))
+                {
+                    this.State.Set(key, value);
+                }
+            }
+
+            this.State.Update(result.Result.Trim());
+
+            foreach (var item in step.NamedOutputs)
+            {
+                if (string.IsNullOrEmpty(item.Key) || item.Key.ToUpperInvariant() == "INPUT" || string.IsNullOrEmpty(item.Value))
+                {
+                    continue;
+                }
+
+                if (item.Key.ToUpperInvariant() == "RESULT")
+                {
+                    this.State.Set(item.Value, result.Result.Trim());
+                }
+                else if (result.Variables.Get(item.Key, out var value))
+                {
+                    this.State.Set(item.Value, value);
+                }
+            }
+            // if (string.IsNullOrEmpty(sequentialPlan.OutputKey))
+            // {
+            //     _ = this.State.Update(result.Result.Trim());
+            // }
+            // else
+            // {
+            //     this.State.Set(sequentialPlan.OutputKey, result.Result.Trim());
+            // }
+
+            // _ = this.State.Update(result.Result.Trim());
+            // if (!string.IsNullOrEmpty(sequentialPlan.OutputKey))
+            // {
+            //     this.State.Set(sequentialPlan.OutputKey, result.Result.Trim());
+            // }
+
+            // if (!string.IsNullOrEmpty(sequentialPlan.ResultKey))
+            // {
+            //     _ = this.State.Get(SkillPlan.ResultKey, out var resultsSoFar);
+            //     this.State.Set(SkillPlan.ResultKey,
+            //         string.Join(Environment.NewLine + Environment.NewLine, resultsSoFar, result.Result.Trim()));
+            // }
+
+            #endregion Update State
+
             this.NextStepIndex++;
-            this.State.Update(context.Result.Trim());
         }
 
         return this;
     }
 
-    protected void SetFunction(ISKFunction function)
+    /// <summary>
+    /// Get JSON representation of the plan.
+    /// </summary>
+    public string ToJson()
+    {
+        return JsonSerializer.Serialize(this);
+    }
+
+    /// <summary>
+    /// To help with reading plans from <see cref="Orchestration.ContextVariables"/>.
+    /// </summary>
+    /// <param name="json">JSON string representation of aPlan</param>
+    /// <param name="context">The context to use for function registrations.</param>
+    /// <returns>An instance of a Plan object.</returns>
+    public static Plan FromJson(string json, SKContext context)
+    {
+        var plan = JsonSerializer.Deserialize<Plan>(json, new JsonSerializerOptions() { IncludeFields = true }) ?? new Plan(string.Empty);
+
+        plan = GetRegisteredFunctions(plan, context);
+
+        return plan;
+    }
+
+    private static Plan GetRegisteredFunctions(Plan plan, SKContext context)
+    {
+        if (plan.Steps.Count == 0)
+        {
+            if (context.IsFunctionRegistered(plan.SkillName, plan.Name, out var skillFunction))
+            {
+                Verify.NotNull(skillFunction, nameof(skillFunction));
+                plan.SetFunction(skillFunction);
+            }
+        }
+        else
+        {
+            foreach (var step in plan.Steps)
+            {
+                GetRegisteredFunctions(step, context);
+            }
+        }
+
+        return plan;
+    }
+
+    private ContextVariables GetNextStepVariables(ContextVariables variables, Plan step)
+    {
+        // If passing to a PLAN, do not use this.Description
+        var defaultInput = step.Steps.Count > 0 ? string.Empty : this.Description ?? string.Empty;
+
+        // Initialize function-scoped ContextVariables
+        // Default input should be the Input from the SKContext, or the Input from the Plan.State, or the Plan.Goal
+        var planInput = string.IsNullOrEmpty(variables.Input) ? this.State.Input : variables.Input;
+        var functionInput = string.IsNullOrEmpty(planInput) ? defaultInput : planInput;
+        var functionVariables = new ContextVariables(functionInput);
+
+        // When I execute a plan, it has a State, ContextVariables, and a Goal
+
+        // Priority for functionVariables is:
+        // - NamedParameters (pull from State by a key value)
+        // - Parameters (pull from ContextVariables by name match, backup from State by name match)
+
+        var functionParameters = step.Describe();
+        foreach (var param in functionParameters.Parameters)
+        {
+            if (variables.Get(param.Name, out var value) && !string.IsNullOrEmpty(value))
+            {
+                functionVariables.Set(param.Name, value);
+            }
+            else if (this.State.Get(param.Name, out value) && !string.IsNullOrEmpty(value))
+            {
+                functionVariables.Set(param.Name, value);
+            }
+        }
+
+        foreach (var item in step.NamedParameters)
+        {
+            if (item.Value.StartsWith("$", StringComparison.InvariantCultureIgnoreCase))
+            {
+                var attrValues = item.Value.Split(new char[] { ',', ';' }, StringSplitOptions.RemoveEmptyEntries);
+                var attrValueList = new List<string>();
+                foreach (var attrValue in attrValues)
+                {
+                    var attr = attrValue.TrimStart('$');
+                    if (variables.Get(attr, out var value) && !string.IsNullOrEmpty(value))
+                    {
+                        attrValueList.Add(value);
+                    }
+                    else if (this.State.Get(attr, out value) && !string.IsNullOrEmpty(value))
+                    {
+                        attrValueList.Add(value);
+                    }
+                }
+
+                functionVariables.Set(item.Key, string.Concat(attrValueList));
+            }
+            else
+            {
+                if (!string.IsNullOrEmpty(item.Value))
+                {
+                    functionVariables.Set(item.Key, item.Value);
+                }
+                else if (variables.Get(item.Value, out var value) && !string.IsNullOrEmpty(value))
+                {
+                    functionVariables.Set(item.Key, value);
+                }
+                else if (this.State.Get(item.Value, out value) && !string.IsNullOrEmpty(value))
+                {
+                    functionVariables.Set(item.Key, value);
+                }
+            }
+        }
+
+        return functionVariables;
+    }
+
+    private void SetFunction(ISKFunction function)
     {
         this.Function = function;
         this.Name = function.Name;
@@ -250,12 +508,21 @@ public class Plan : ISKFunction
         this.RequestSettings = function.RequestSettings;
     }
 
-    protected ISKFunction? Function { get; set; } = null;
+    /// <summary>
+    /// Internal constant string representing the plan key.
+    /// </summary>
+    internal const string PlanKey = "PLAN__PLAN__KEY";
 
-    private List<ISKFunction> _steps = new();
+    [JsonPropertyName("next_step_index")]
+    public int NextStepIndex { get; internal set; } = 0;
+
+    private ISKFunction? Function { get; set; } = null;
+
+    private readonly List<Plan> _steps = new();
 
     private static void AddVariablesToContext(ContextVariables vars, SKContext context)
     {
+        // Loop through State and add anything missing to functionContext
         foreach (var item in vars)
         {
             if (!context.Variables.ContainsKey(item.Key))
---
+++ dotnet/src/SemanticKernel/Planning/Planners/FunctionFlowPlanner.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel.CoreSkills;
+using Microsoft.SemanticKernel.Diagnostics;
+using Microsoft.SemanticKernel.Orchestration;
+
+namespace Microsoft.SemanticKernel.Planning.Planners;
+
+/// <summary>
+/// A planner that uses the Function Flow semantic function to create a plan.
+/// </summary>
+public class FunctionFlowPlanner
+{
+    /// <summary>
+    /// Initialize a new instance of the <see cref="FunctionFlowPlanner"/> class.
+    /// </summary>
+    /// <param name="kernel">The semantic kernel instance.</param>
+    /// <param name="config">The planner configuration.</param>
+    public FunctionFlowPlanner(IKernel? kernel, PlannerConfig? config = null)
+    {
+        Verify.NotNull(kernel, $"{this.GetType().FullName} requires a kernel instance.");
+        if (config == null)
+        {
+            config = new PlannerConfig();
+            config.ExcludedFunctions.Add(RestrictedSkillName);
+        }
+
+        this.Config = config;
+
+        this._functionFlowFunction = kernel.CreateSemanticFunction(
+            promptTemplate: SemanticFunctionConstants.FunctionFlowFunctionDefinition,
+            skillName: RestrictedSkillName,
+            description: "Given a request or command or goal generate a step by step plan to " +
+                         "fulfill the request using functions. This ability is also known as decision making and function flow",
+            maxTokens: this.Config.MaxTokens,
+            temperature: 0.0,
+            stopSequences: new[] { "<!--" });
+
+        this._context = kernel.CreateNewContext();
+    }
+
+    /// <summary>
+    /// Create a plan for a goal.
+    /// </summary>
+    /// <param name="goal">The goal to create a plan for.</param>
+    /// <returns>The plan.</returns>
+    public async Task<Plan> CreatePlanAsync(string goal)
+    {
+        string relevantFunctionsManual = await this._context.GetFunctionsManualAsync(goal, this.Config);
+        this._context.Variables.Set("available_functions", relevantFunctionsManual);
+
+        this._context.Variables.Update(goal);
+
+        var planResult = await this._functionFlowFunction.InvokeAsync(this._context);
+
+        string fullPlan = $"<{FunctionFlowParser.GoalTag}>\n{goal}\n</{FunctionFlowParser.GoalTag}>\n{planResult.Result.Trim()}";
+
+        var plan = fullPlan.ToPlanFromXml(this._context);
+
+        return plan;
+    }
+
+    protected PlannerConfig Config { get; }
+
+    private readonly SKContext _context;
+
+    /// <summary>
+    /// the function flow semantic function, which takes a goal and creates an xml plan that can be executed
+    /// </summary>
+    private readonly ISKFunction _functionFlowFunction;
+
+    /// <summary>
+    /// The name to use when creating semantic functions that are restricted from the PlannerSkill plans
+    /// </summary>
+    private const string RestrictedSkillName = "PlannerSkill_Excluded"; // todo we've got copies of this now
+}
