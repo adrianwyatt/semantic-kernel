21e1831deb1f27830b4b1b6d105f242c8a009672
Adrian Bonar (HE/HIM) <Adrian.Bonar@microsoft.com>
2023-08-08 15:08:39 -0700

    Wip

---
+++ dotnet/src/Skills/Skills.UnitTests/FirstPartyPlugin/FirstPartyPluginUnitTests.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using System.Threading.Tasks;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Skills.FirstPartyPlugin;
+using Xunit;
+
+namespace SemanticKernel.Skills.UnitTests.FirstPartyPlugin;
+public class FirstPartyPluginUnitTests
+{
+    [Fact]
+    public async Task FooTestAsync()
+    {
+        IKernel kernel = new KernelBuilder()
+            .Build();
+
+        var foo = await kernel.ImportFirstPartyPluginAsync(
+            "skillName",
+            "C:\\src\\adrianwyatt-semantic-kernel\\dotnet\\src\\Skills\\Skills.MS1P\\flux_semantic_kernel.json");
+    }
+}
